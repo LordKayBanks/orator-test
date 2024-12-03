608c7fe9c1fdd0800c61812828aa87ccd51be7a6
Kayode Ogidan <kaybanks.developer@gmail.com>
2024-12-03 07:03:31 +0100

    Updates

--- assets/body.override.style-ASw01Fc3.js
+++ assets/body.override.style-ASw01Fc3.js
@@ -8,6 +8,12 @@ body {
    padding-top: 60px !important;
    padding-bottom: 60px !important;
 
+   // height: 100vh !important; /* Fallback for older browsers */
+   // height: 100svh !important; /* Small viewport height */
+   // height: 100dvh !important; /* Dynamic viewport height */
+   // margin: 0 !important;
+   // overflow: hidden !important;
+
    // box-sizing: border-box;
    // word-wrap: break-word;
    // line-break: strict;
